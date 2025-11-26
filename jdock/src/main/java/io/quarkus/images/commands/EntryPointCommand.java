477a05b686aa9d1d5af585983bf758fb2a37d983
Guillaume Smet <guillaume.smet@gmail.com>
2025-11-26 11:52:10 +0100

    Make EntryPointCommand a bit more flexible
    I did it in one of my (failed) experiments and it looks useful so
    keeping it.

--- jdock/src/main/java/io/quarkus/images/commands/EntryPointCommand.java
+++ jdock/src/main/java/io/quarkus/images/commands/EntryPointCommand.java
@@ -2,17 +2,19 @@ package io.quarkus.images.commands;
 
 import io.quarkus.images.BuildContext;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class EntryPointCommand implements Command {
 
-    private final String cmd;
+    private final String[] cmd;
 
-    public EntryPointCommand(String cmd) {
+    public EntryPointCommand(String[] cmd) {
         this.cmd = cmd;
-
     }
 
     @Override
     public String execute(BuildContext context) {
-        return "ENTRYPOINT [\"" + cmd + "\"]";
+        return "ENTRYPOINT [" + Arrays.stream(cmd).collect(Collectors.joining("\", \"", "\"", "\"")) + "]";
     }
 }
